543031d3ff8a406c1e8af248f6ede3582b2d24ed
doitino <doitino@users.noreply.github.com>
2021-09-15 22:11:08 +0700

    check email check out

--- TravelTour/src/app/DatTour/thanh-toan/thanh-toan.component.ts
+++ TravelTour/src/app/DatTour/thanh-toan/thanh-toan.component.ts
@@ -44,14 +44,29 @@ export class ThanhToanComponent implements OnInit {
   }
 
   public checkout(nameCustomer: any, phoneCustomer: any, emailCustomer: any, addressCustomer: any) {
-    if(nameCustomer ==null || phoneCustomer == null || emailCustomer == null || addressCustomer == null){
-      alert("Vui lòng nhập đầy đủ thông tin cần thiết!");
-    }
-    else {
-      alert("Đã hoàn tất đặt tour");
-      this.router.navigate(['trangchu']);
-    }
 
+    if (phoneCustomer == null || nameCustomer == null || addressCustomer == null || emailCustomer ==null) {
+      alert("Vui lòng nhập đầy đủ thông tin");
+      return false;
+    } else {
+      var at = emailCustomer.indexOf("@");
+      var dot = emailCustomer.lastIndexOf(".");
+      var space = emailCustomer.indexOf(" ");
+      if ((at != -1) && //có ký tự @
+        (at != 0) && //ký tự @ không nằm ở vị trí đầu
+        (dot != -1) && //có ký tự .
+        (dot > at + 1) && (dot < emailCustomer.length - 1) //phải có ký tự nằm giữa @ và . cuối cùng
+        &&
+        (space == -1)) //không có khoẳng trắng
+      {
+        alert("Hoàn tất đặt tour");
+        return true;
+      } else {
+        alert("Địa chiemail không hợp lệ");
+        return false;
+      }
 
+    }
   }
+
 }
